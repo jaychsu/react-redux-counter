fd12fea16bd8d2a303c5fde6c7950dc8a81592dd
Jaych Su <hi.jaychsu@gmail.com>
2019-04-21 08:59:55 +0800

    Kick build

---
+++ build/precache-manifest.243b809c4090a4e51608c87172986691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "72ddf9cee784b783f9c3",
+    "url": "/static/js/main.a621c0aa.chunk.js"
+  },
+  {
+    "revision": "7daf413cf451efa0334c",
+    "url": "/static/js/2.4b15128e.chunk.js"
+  },
+  {
+    "revision": "72ddf9cee784b783f9c3",
+    "url": "/static/css/main.94da0b22.chunk.css"
+  },
+  {
+    "revision": "07e1d30e6bf14761634da370f3ccd8f8",
+    "url": "/index.html"
+  }
+];
